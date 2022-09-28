a6badcde767c2706811275000c86634decc576ee
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-09-27 20:40:27 -0500

    importing api information

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3b5631a6d3b401b148c3bbbeba98b054&page=1'
 
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
@@ -11,11 +12,49 @@ const search = document.getElementById('search')
 //Get initial movies
 getMovies(API_URL)
 
-async function getMovies (url) {
+async function getMovies(url) {
     const res = await fetch(url)
     const data = await res.json()
 
-    console.log(data.results)
+    showMovies(data.results)
+}
+
+function showMovies(movies){
+    main.innerHTML = ''
+
+    movies.forEach((movie) => {
+        const {title, poster_path, vote_average, overview} = movie
+
+        const movieEL = document.createElement('div')
+        movieEL.classList.add('movie')
+
+        movieEL.innerHTML = `  
+        
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                ${overview}
+            </div>
+        
+        `
+        main.appendChild(movieEL)
+
+    })
+
+}
+
+function getClassByRate(vote){
+    if(vote >= 8) {
+        return 'green'
+    } else if(vote >= 5) {
+        return'orange'
+    } else {
+        return 'red'
+    }
 }
 
 form.addEventListener('submit', (e) => {
